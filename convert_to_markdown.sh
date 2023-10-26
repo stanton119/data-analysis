a091dd4d61ae4472e6117ae1602ecbd02b5f03ef
stanton119 <stanton1191@gmail.com>
2023-10-26 23:02:30 +0100

    action update

--- convert_to_markdown.sh
+++ convert_to_markdown.sh
@@ -5,7 +5,7 @@ echo $2
 
 # ignore deleted files, filter for .ipynb only, filter for last 10 commits
 # files=$(git diff --name-only --diff-filter=d $1 $2 | grep .ipynb)
-files=$(git diff --name-only origin/master..HEAD | grep .ipynb | grep -v 'unfinished')
+files=$(git diff --name-only origin/master~10..HEAD | grep .ipynb | grep -v 'unfinished')
 for i in $files;
 do
    echo $i
