76429248a89ad82231cff2f01cee6eedf3cf0a06
stanton119 <stanton1191@gmail.com>
2023-10-26 22:45:10 +0100

    action update

--- convert_to_markdown.sh
+++ convert_to_markdown.sh
@@ -4,7 +4,8 @@ echo $1
 echo $2
 
 # ignore deleted files, filter for .ipynb only, filter for last 10 commits
-files=$(git diff --name-only --diff-filter=d $1 $2 | grep .ipynb)
+# files=$(git diff --name-only --diff-filter=d $1 $2 | grep .ipynb)
+files=$(git diff --name-only HEAD^..HEAD | grep .ipynb)
 for i in $files;
 do
    echo $i
