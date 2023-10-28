8ae38d76d58519e47813c13eda7a9402e710b803
stanton119 <stanton1191@gmail.com>
2023-10-28 22:06:43 +0100

    action update

--- convert_to_markdown.sh
+++ convert_to_markdown.sh
@@ -3,9 +3,14 @@
 echo $1
 echo $2
 
+# only convert newly changed files?
 # ignore deleted files, filter for .ipynb only, filter for last 10 commits
-# files=$(git diff --name-only --diff-filter=d $1 $2 | grep .ipynb)
-files=$(git diff --name-only origin/master~10..HEAD | grep .ipynb | grep -v 'unfinished')
+if [ "$1" = "diff" ]; then
+   files=$(git diff --name-only origin/master~10..HEAD | grep .ipynb | grep -v 'unfinished')
+else
+   files=$(find . -type f -name "*.ipynb" | grep .ipynb | grep -v 'unfinished')
+fi
+
 for i in $files;
 do
    echo $i
